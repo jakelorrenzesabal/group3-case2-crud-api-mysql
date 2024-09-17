0ceb08ba99e59cab3c6e8f57f8a97b60efbf12dd
jakesabal <jakelorenzecan@gmail.com>
2024-09-17 09:31:36 +0800

    first commit

---
+++ _middleware/error-handler.js
@@ -0,0 +1,15 @@
+module.exports = errorHandler;
+
+function errorHandler(err, req, res, next) {
+    switch (true) {
+        case typeof err === 'string':
+
+            const is404 = err.toLowerCase().endsWith('not found');
+            const statusCode = is404 ? 404 : 400;
+            return res.status(statusCode).json({message: err });
+            case err.message && err.message.toLowerCase().includes('deactivated'):
+                return res.status(403).json({ message: 'Account is deactivated' });
+        default:
+            return res.status(500).json({ message: err.message });
+    }
+}
---
+++ server.js
@@ -0,0 +1,18 @@
+require('rootpath')();
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const errorHandler = require('_middleware/error-handler');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use('/users', require('./users/users.controller'));
+app.use('/api/users', require('./users/users.controller'));
+
+app.use(errorHandler);
+
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+app.listen(port, () => console.log('Server listening on port ' + port));
